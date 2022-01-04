312486382c44bdf68b6963299a9be77c527e6f90
FabioCividini <82501787+FabioCividini@users.noreply.github.com>
2022-01-04 15:35:20 +0100

    AGGIUNTO COMMENTO GRUPPO

--- spring-mvc-best-2021/src/main/java/org/generation/italy/controller/IndexController.java
+++ spring-mvc-best-2021/src/main/java/org/generation/italy/controller/IndexController.java
@@ -7,6 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+//GRUPPO JEROME,SERGIO,FABIO,MICHELE,MARCO,MATTIA M
+
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
